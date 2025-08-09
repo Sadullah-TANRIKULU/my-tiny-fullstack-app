225118910712334576a49c59243519ad88667d7d
Sadullah-TANRIKULU <sadullahtanrikulu@gmail.com>
2025-08-09 13:06:10 +0200

    not hard-coded port

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,5 @@ app.delete('/delete', async (req, res) => {
   res.json({ status: 'deleted' });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
